d96d75c93907668fbebc71359386cfc26a43e95b
Octavio Benitez <tatokinneas@gmail.com>
2024-04-24 03:51:24 -0600

    Feat: Test request to ExchangeRate

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        boolean menuSelection = true;
+        String menuText = "|*************************************|" +
+                    "Bienvenido";
+        Scanner sc = new Scanner(System.in);
+        System.out.println(menuText);
+        String url_api = "https://v6.exchangerate-api.com/v6/146601e2c21bb2ec0a2a430f/pair/USD/MXN/1";
+        while(menuSelection){
+            int option = sc.nextInt();
+            if (option==0){
+                menuSelection=false;
+            }
+            if(option==1){
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(url_api))
+                                .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+                System.out.println(response.body());
+            }
         }
     }
 }
